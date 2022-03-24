fad75c3280e0621f9e09212be006735f652bc303
Andrew Colin <acolin@c1r2p2.42nice.fr>
2022-03-24 15:10:20 +0100

    finish

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -19,8 +19,8 @@ int main(void)
 	Bureaucrat *A = new Bureaucrat("Rober", 20);
 	std::cout << A << std::endl;
 	
-	Bureaucrat *M = new Bureaucrat("Luc", 40);
-	std::cout << M << std::endl;
+	Bureaucrat *B = new Bureaucrat("Luc", 40);
+	std::cout << B << std::endl;
 
 	try {
 		Form *formA = new Form("Adhesion", 30, 20);
@@ -30,18 +30,18 @@ int main(void)
 		std::cout << formA << std::endl;
 		delete formA;
 
-		Form *formM = new Form("Adhesion2", 30, 20);
-		std::cout << formM << std::endl;
+		Form *formB = new Form("Adhesion2", 30, 20);
+		std::cout << formB << std::endl;
 
-		M->signForm(formM);
-		std::cout << formM << std::endl;
-		delete formM;
+		B->signForm(formB);
+		std::cout << formB << std::endl;
+		delete formB;
 	} catch (Form::GradeTooLowException &e) {
 		std::cout << "Error: " << e.what() << std::endl;	
 	}
 	
 	delete A;
-	delete M;
+	delete B;
 	
 	return (0);
 }
--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -18,14 +18,14 @@ int main(void)
 	Intern A;
 	
 	try {
-		Form*	form = A.makeForm("robotomy request", "Bender");
+		Form*	form = A.makeForm("robotomy request", "r2d2");
 		delete form;
 	} catch (Intern::FormDoesNotExistException &e) {
 		std::cout << "Error: " << e.what() << std::endl;	
 	}
 
 	try {
-		Form*	form = A.makeForm("robotomy test", "Bender");
+		Form*	form = A.makeForm("robotomy test", "r2d2");
 		delete form;
 	} catch (Intern::FormDoesNotExistException &e) {
 		std::cout << "Error: " << e.what() << std::endl;	
